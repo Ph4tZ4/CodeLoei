2e04118ac0f301eaa445fe78e25656b6d91ffad8
Ph4tZ4 <Kittiphat240449@gmail.com>
2026-01-13 22:34:07 +0700

    TS

--- client/src/contexts/LanguageContext.tsx
+++ client/src/contexts/LanguageContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, type ReactNode } from 'react';
 import { th } from '../locales/th';
 import { en } from '../locales/en';
 
